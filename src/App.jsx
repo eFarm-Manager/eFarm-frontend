b97f73310976acc39b03272d8de44801d38e205a
M3m0ry915 <107813607+M3m0ry915@users.noreply.github.com>
2024-10-13 21:14:34 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
 
     }, []);
 
-    const handleLogin = () => {
+    const handleLogin = (expireCodeInfoFromLogin) => {
         setIsAuthenticated(true);
         const roles = sessionStorage.getItem('roles');
         if (roles) {
